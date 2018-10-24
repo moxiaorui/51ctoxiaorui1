9006aae2722adc90c1953cb6f052f13f5e9ad5d4
moxiaorui <2123018048@qq.com>
2018-10-25 00:54:18 +0800

    first ml KNN

---
+++ lesson_03/knn.py
@@ -0,0 +1,14 @@
+#!usr/bin/python
+# encoding:utf-8
+
+
+'''
+author:xiaorui
+contact:2123018048@qq.com
+@file:knn.py
+@time:2018/10/25 0025 0:50
+@welcome to learn ai
+'''
+class KNN(object):
+    def__init__(self):
+       pass
